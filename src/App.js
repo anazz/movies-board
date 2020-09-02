7dafdd5c4e3555ab7818375cab3b88376b60a3cb
Ana Zegheanu <ana.zegheanu@gmail.com>
2020-09-02 17:22:01 +0200

    basic temporary layout change movie info component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './components/Home/Home';
 import MoviesLibrary from './components/MoviesLibrary/MoviesLibrary';
 import SelectedMovie from './components/SelectedMovie/SelectedMovie';
 import AddMovie from './components/AddMovie/AddMovie';
+import ChangeMovieInfo from './components/ChangeMovieInfo/ChangeMovieInfo';
 
 function App() {
   return (
---
+++ src/components/ChangeMovieInfo/ChangeMovieInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ChangeMovieInfo.css';
+
+const ChangeMovieInfo = (props) => {
+    return (
+        <div className="wrapper">
+            <h2>Change Movie Info</h2>
+        </div>    
+    );
+};
+
+export default ChangeMovieInfo;
