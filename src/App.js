995e14c1f2c385cb7ca7022ca44ebb608854da96
Ana Zegheanu <ana.zegheanu@gmail.com>
2020-09-02 17:49:35 +0200

    app routes update with fragment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
@@ -16,26 +16,42 @@ function App() {
         <Navbar />   
           <div className='container'>
             <Switch>
-              <Route exact path='/' component={Home} /> 
+              <Route exact path='/' component={Home} />
+              <Route
+                exact
+                path='/movies'
+                render={(props)=>
+                  <Fragment>
+                    <MoviesLibrary prods={MoviesLibrary}/>
+                  </Fragment>
+                }
+              />
               <Route
                 exact
                 path='/movie'
-
-              />
-              <Route
-              exact
-              path='/movies'
-
+                render={(props)=>
+                  <Fragment>
+                    <SelectedMovie prods={SelectedMovie}/> 
+                  </Fragment>
+                }
               />
               <Route
                 exact
                 path='/add'
-
+                render={(props)=>
+                  <Fragment>
+                    <AddMovie prods={AddMovie}/>
+                  </Fragment>
+                }
               />
               <Route
                 exact
                 path='/change'
-
+                render={(props)=>
+                  <Fragment>
+                    <ChangeMovieInfo prods={ChangeMovieInfo}/>                
+                  </Fragment>
+                }  
               />
             </Switch>
          </div>
