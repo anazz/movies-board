96986be70d0be23d81816230d274c04f0129146a
Ana Zegheanu <ana.zegheanu@gmail.com>
2020-09-02 16:55:33 +0200

    basic temporary layout home component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div className="home-section">
+            <div className="app-main-title">
+                <h1>Movie Board</h1>
+            </div>
+            <div className="app-description">
+                <p>A site for a simpler movie search</p>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
