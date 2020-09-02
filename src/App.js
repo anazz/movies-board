10c061dd03ac39cee13f5a27a22f03d7aaacb660
Ana Zegheanu <ana.zegheanu@gmail.com>
2020-09-02 17:11:53 +0200

    basic temporary layout selected movie component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
 import MoviesLibrary from './components/MoviesLibrary/MoviesLibrary';
+import SelectedMovie from './components/SelectedMovie/SelectedMovie';
 
 function App() {
   return (
---
+++ src/components/SelectedMovie/SelectedMovie.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SelectedMovie.css';
+
+const SelectedMovie = (props) => {
+    return (
+        <div className="wrapper">
+            <h2>Selected Movie</h2>
+        </div>
+    );
+};
+
+export default SelectedMovie;
