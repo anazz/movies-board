1f958140dacc0ed75dfbd06a5057c81d7168e51c
Ana Zegheanu <ana.zegheanu@gmail.com>
2020-09-03 21:20:15 +0200

    sent data from server to the selected movie component

--- src/App.js
+++ src/App.js
@@ -56,7 +56,9 @@ function App() {
                 path='/movie'
                 render={()=>
                   <Fragment>
-                    <SelectedMovie prods={SelectedMovie}/> 
+                    <SelectedMovie prods={SelectedMovie}
+                      movies={movies}
+                    /> 
                   </Fragment>
                 }
               />
--- src/components/SelectedMovie/SelectedMovie.js
+++ src/components/SelectedMovie/SelectedMovie.js
@@ -2,9 +2,15 @@ import React from 'react';
 import './SelectedMovie.css';
 
 const SelectedMovie = (props) => {
+
+    const movieSelected = props.movies.map((movie) => (
+        console.log(movie)
+    ));
+    
     return (
         <div className="wrapper">
             <h2>Selected Movie</h2>
+            <div className="movie-list">{movieSelected}</div>
         </div>
     );
 };
