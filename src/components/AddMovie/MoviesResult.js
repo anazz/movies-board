314f325eff6793278c57d1767d46544e972ba0b9
Ana Zegheanu <ana.zegheanu@gmail.com>
2020-09-06 12:11:38 +0200

    code deconstrustion & creation of movies results component & of add movie form component

---
+++ src/components/AddMovie/MoviesResult.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MoviesResults = (props) => {
+	return (
+		<li className="results-list">
+            <h3>??</h3>
+            <span className="release_date">???</span>
+		</li>
+	);
+};
+
+export default MoviesResults;
