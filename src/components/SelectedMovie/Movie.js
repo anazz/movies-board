eddf72b03be7090aee1934eb19a6246a188effb9
Ana Zegheanu <ana.zegheanu@gmail.com>
2020-09-07 13:20:04 +0200

    added layout to the movie selected component and some style update

--- src/components/SelectedMovie/Movie.js
+++ src/components/SelectedMovie/Movie.js
@@ -1,13 +1,44 @@
 import React from 'react';
+import bootstrap from 'bootstrap';
+import { Slide } from 'react-slideshow-image';
+import './Movie.css';
 
 const Movie = (props) => {
 	return (
-        <div>
-            <h3>{props.movie.title}</h3>
-            <img src={props.movie.poster} alt="movie poster" />
-            <span className="release_date">{props.movie.release_date}</span>
-            <p className="description">{props.movie.description}</p>
+        <div className="">
+            {/* <img className="movie-backdrop" src="" alt="" /> */}
+            <div className="row movie-info">
+                <div className="col">
+                    {/* <img src={props.movie.backdrop} alt="movie poster" className="bg-poster" /> */}
+                    <div className="movie-top-info">
+                    <   span className="release_date">Sortie en salles: {props.movie.release_date}</span>
+                        <h3>{props.movie.title}</h3>
+                        {/* <img src={props.movie.poster} alt="movie poster" /> */}
+                        {/* <p className="description">{props.movie.description}</p> */}
+                    </div>
+                </div>
+                <div className="col">
+                    <img src={props.movie.poster} alt="movie poster" className="main-poster"/>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-6 description-wrapper">
+                <p className="description">{props.movie.description}</p>
+                </div>
+            </div>
+            <div className="row similar-movies-wrapper">
+                <div className="col">
+                    <ul>
+                    {props.movie.similar_movies.map(similar_movie => (
+                        <li>
+                            <img src= {similar_movie.poster} alt="similar movie poster" className="similar-movie-poster"/>
+                        </li>
+                    ))}
+                    </ul>
+                </div>
+            </div>
         </div>
+        
 	);
 };
 
--- src/components/SelectedMovie/SelectedMovie.js
+++ src/components/SelectedMovie/SelectedMovie.js
@@ -3,20 +3,32 @@ import Movie from './Movie';
 import './SelectedMovie.css';
 
 const SelectedMovie = (props) => {
-
-    const movieSelected = props.movies.map((movie) => (
+    
+const movieSelected = props.movies.map((movie) => (
         console.log(movie),
         <Movie
-			movie={movie}
             key={movie.id}
+			movie={movie}
 		/>
     ));
     
     return (
-        <div className="wrapper">
-            <h2>Selected Movie</h2>
-            <div className="movie-list">{movieSelected}</div>
-        </div>
+            <div className="movie-wrapper">
+                <div className="bg-wrapper">
+                    <div className="bg">
+                    {props.movies.map((movie) => (
+                    <img src={movie.backdrop} alt="movie poster" className="bg-poster" />
+                    ))}
+                    <div className="bg-layer"></div> 
+                    </div>              
+                </div>
+                        
+                <h2 className="movie-page-title">Selected Movie</h2>
+                <div className="movie-section">
+                    <div className="bg-section"></div>
+                    {movieSelected}
+                </div>
+            </div>
     );
 };
 
